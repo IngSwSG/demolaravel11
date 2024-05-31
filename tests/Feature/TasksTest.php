93f917fdb09ded6a8e56c311c03cc2a3349466b0
Jeferson Espinoza <jef551@hotmail.com>
2024-05-31 14:22:44 -0600

    Jeferson Espinoza C12676

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -6,7 +6,7 @@ use App\Models\User;
 it('muestra la informacion de una tarea', function () {
     $task = Task::factory()->create([
         'name' => 'Tarea nueva'
-    
+
     ]);
 
     $response = $this->get($task->path());
@@ -17,7 +17,7 @@ it('muestra la informacion de una tarea', function () {
 
 it('crea una nueva tarea', function (){
     $this->withoutExceptionHandling();
-    
+
     $user = User::factory()->create();
 
     $data = [
@@ -42,7 +42,7 @@ it('actualizar una tarea', function () {
    $task = Task::factory()->create([
        'name' => 'Tarea vieja'
    ]);
-   
+
    $data = [
        'name' => 'Tarea actualizada',
        'user_id' => $task->user_id
@@ -65,9 +65,33 @@ it('actualizar el usuario de una tarea', function () {
         'name' => 'Tarea vieja',
         'user_id' => $otroUsuario->id
     ];
- 
+
     $response = $this->put($task->path(), $data);
- 
+
     expect($task->fresh()->user_id)->toBe($otroUsuario->id);
- 
+
  });
+
+
+ it('ProbarFiltradoDeUsuarios', function () {
+
+    // Crear dos instancias de usuario
+    $usuario1 = User::factory()->create();
+    $usuario2 = User::factory()->create();
+
+    // Generar tareas asociadas a cada usuario
+    $tarea1 = Task::factory()->create(['user_id' => $usuario1->id, 'name' => 'Comprar pan']);
+    $tarea2 = Task::factory()->create(['user_id' => $usuario1->id, 'name' => 'Comprar Natilla']);
+    $tarea3 = Task::factory()->create(['user_id' => $usuario2->id, 'name' => 'Hacer cafe']);
+
+    // Realizar una solicitud a la ruta de índice aplicando el filtro por user_id
+    $respuesta = $this->get(route('tasks.index', ['user_id' => $usuario1->id]));
+
+    // Verificar que la tarea del usuario1 está presente en la respuesta
+    $respuesta->assertSee($tarea1->name);
+    $respuesta->assertSee($tarea2->name);
+
+    // Verificar que la tarea del usuario2 no está presente en la respuesta
+    $respuesta->assertDontSee($tarea3->name);
+
+});
