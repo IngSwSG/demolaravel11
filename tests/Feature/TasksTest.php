f803d42a8e0afa9cb655b8086324d2f15e66d42a
Marcos <marcosgonzana18@gmail.com>
2024-05-31 13:45:01 -0600

    Marcos-c13331

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -71,3 +71,34 @@ it('actualizar el usuario de una tarea', function () {
     expect($task->fresh()->user_id)->toBe($otroUsuario->id);
  
  });
+
+ it('filtra las tareas por usuario', function () {
+    
+    $user1 = User::factory()->create();
+    $user2 = User::factory()->create();
+
+   
+    $task1 = Task::factory()->create([
+        'name' => 'Tarea del usuario 1',
+        'user_id' => $user1->id
+    ]);
+
+    
+    $task2 = Task::factory()->create([
+        'name' => 'Tarea del usuario 2',
+        'user_id' => $user2->id
+    ]);
+
+   
+    $response = $this->get('/tasks?user_id=' . $user1->id);
+
+    $response->assertStatus(200);
+    $response->assertSee('Tarea del usuario 1');
+    $response->assertDontSee('Tarea del usuario 2');
+    
+    $response = $this->get('/tasks?user_id=' . $user2->id);
+
+    $response->assertStatus(200);
+    $response->assertSee('Tarea del usuario 2');
+    $response->assertDontSee('Tarea del usuario 1');
+});
