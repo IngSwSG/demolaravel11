5ca56099123ffb43ec09485d703ee77f8b8c246a
DiegoReyes <diereyescar@gmail.com>
2024-05-31 14:27:43 -0600

    B96469

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -69,5 +69,26 @@ it('actualizar el usuario de una tarea', function () {
     $response = $this->put($task->path(), $data);
  
     expect($task->fresh()->user_id)->toBe($otroUsuario->id);
+    
  
  });
+ it('filtrar las tareas por usuario', function () {
+    $user1 = User::factory()->create();
+    $user2 = User::factory()->create();
+
+    $task1 = Task::factory()->create([
+        'name' => 'Tarea del usuario 1',
+        'user_id' => $user1->id
+    ]);
+
+    $task2 = Task::factory()->create([
+        'name' => 'Tarea del usuario 2',
+        'user_id' => $user2->id
+    ]);
+
+    $response = $this->get(route('tasks.index', ['user_id' => $user1->id]));
+
+    $response->assertStatus(200);
+    $response->assertSee('Tarea del usuario 1');
+    $response->assertDontSee('Tarea del usuario 2');
+});
