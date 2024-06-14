8036f3e21d37dd147b4868a94c53184aa9854abc
xinia <xinia@XINIABP>
2024-06-14 15:10:07 -0600

    Xinia

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -13,23 +13,29 @@ class Team extends Model
     public function add($users)
     {
 
-        $this->guardAgainstTooManyMembers();
+
 
         if ($users instanceof User) {
+            $this->guardAgainstTooManyMembers(1);
             return $this->users()->save($users);
         }
 
-        $this->users()->saveMany($users);
+        if ($users instanceof \Illuminate\Database\Eloquent\Collection ) {
+            $this->guardAgainstTooManyMembers($users->count());
+            return $this->users()->saveMany($users);
+
+        }
     }
+
 
     public function users()
     {
         return $this->hasMany(User::class);
     }
 
-    protected function guardAgainstTooManyMembers()
+    protected function guardAgainstTooManyMembers($newMembersCount)
     {
-        if ($this->users()->count() >= $this->size) {
+        if (($this->users()->count() + $newMembersCount) > $this->size) {
             throw new Exception();
         }
     }
--- tests/Feature/TeamsTest.php
+++ tests/Feature/TeamsTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Team;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 it('un equipo puede agrear usuarios', function(){
     $team = Team::factory()->create();
@@ -14,11 +15,12 @@ it('un equipo puede agrear usuarios', function(){
     expect($team->users)->count()->toBe(2);
 });
 
+
 it('un equipo puede tener un tamaño maximo', function(){
     $team = Team::factory()->create(['size' => 2]);
     $user1 = User::factory()->create();
     $user2 = User::factory()->create();
-    
+
     $team->add($user1);
     $team->add($user2);
 
@@ -28,15 +30,18 @@ it('un equipo puede tener un tamaño maximo', function(){
     $user3 = User::factory()->create();
     $team->add($user3);
 
-
 });
 
-it('un equipo puede agregar multiples usuarios a la vez', function(){
+
+it('un equipo puede agregar multiples usuarios a la vez (Prueba de regresion Solucionada)', function(){
+
+    $this->expectException(Exception::class);
     $team = Team::factory()->create(['size' => 3]);
-    $users = User::factory(3)->create();
+    $users = User::factory(4)->create();
 
     $team->add($users);
 
     expect($team->users)->count()->toBe(3);
 });
+
 
