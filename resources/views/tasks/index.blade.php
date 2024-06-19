96576bef9def9a018a27c760eda2ccb08cc1990a
“stivesr” <gilberthsolisrios@gmail.com>
2024-06-19 14:24:47 -0600

    merge cambios

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
     <title>Tareas</title>
 </head>
 <body>
-    <h1>Tareas</h1>
-    <a href="{{ route('tasks.create') }}">Crear</a>
+    <h1 style="color:red;">Tareas</h1>
+    <a href="/tasks/create" style="color:indigo;">Crear</a>
     <form action="{{ route('tasks.index') }}" method="GET">
         <input type="text" name="search" value="{{ $search }}">
         <select name="user_id" id="user_id">
@@ -33,17 +32,6 @@
                     <span>Completada</span>
                 @endif
             </li>
-=======
-<h1 style="color:red;">Tareas</h1>
-<a href="/tasks/create" style="color:indigo;">Crear</a>
-<form action="{{ route('tasks.index') }}">
- 
-    <input type="text" name="search" value="{{ $search }}">
-    <select name="user_id" id="user_id">
-        <option value="">Todos los usuarios</option>
-        @foreach ($users as $user)
-            <option value="{{ $user->id }}" {{ $user->id == request('user_id') ? 'selected' : '' }}>{{ $user->name }}</option>
->>>>>>> 0d39ade7ef98457ae8d67e5ba7ddd5fe67c0d075
         @endforeach
     </ul>
 </body>
