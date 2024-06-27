3af8f8768a6f299ca102dfed906e99bd75b738d4
Pendragon <66645373+PENDRAGON2121@users.noreply.github.com>
2024-06-27 15:54:53 -0600

    C16236

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,6 +38,7 @@ class DatabaseSeeder extends Seeder
         Task::factory(2)->create([
             'user_id' => $user2->id,
         ]);
+        
         Schema::enableForeignKeyConstraints();
     }
 }
